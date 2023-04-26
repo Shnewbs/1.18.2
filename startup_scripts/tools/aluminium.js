50c4c0fa35e040f774ed624c5a84f91be724c84c
liquidblur <90636566+Shnewbs@users.noreply.github.com>
2023-04-26 14:46:04 -0700

    Update aluminium.js

--- startup_scripts/tools/aluminium.js
+++ startup_scripts/tools/aluminium.js
@@ -8,17 +8,44 @@ console.info('TFC Greggy Tools Working Flawlessly)')
 
 onEvent('item.registry', event => {
     var QuickMetalChange = ["aluminium"]
-    var tools = [`chisel`,`hammer`,`hoe`,`javalin`,`knife`,`mace`,`pickaxe`,`propick`,`saw`,`scythe`,`shovel`,`sword`,`chain`,`wrench`,`butchery_knife`,`buzzsaw`,`chainsaw`,`crowbar`,`drill`,`file`,`mallet`,`plunger`,`screwdriver`,`wire_cutter`]
+    var toolBlade = [`sword`,`knife`,`buzzsaw`,`chainsaw`]
+    var toolHeads = [/*`chisel`,*/`hammer`,`hoe`,`javelin`,`mace`,`pickaxe`,`propick`,`saw`,`scythe`,`shovel`,`branch_cutter`,`file`,`plunger`,`screwdriver`,/*`electric_screwdriver`,`electric_wrench`,*/`wire_cutter`]
+    var toolWithout = [`chain`,`crowbar`,`butchery_knife`,`wrench`]
 
+//====================================================================================================================================================================\\
 
+QuickMetalChange.forEach(material => {
+toolHeads.forEach(th => {
+    let displaytool = capfirst(`${th}`)
+    let displayMaterial = capfirst(`${material}`)
 
-    tools.forEach(tool => {
-        QuickMetalChange.forEach(material => {
-            let displayMaterial = capfirst(`${material}`)
-            let displaytool = capfirst(`${tool}`)
+    event.create(`${th}_head_${material}`).displayName(`${displayMaterial} ${displaytool} Head`).texture(`kubejs:item/tools/${th}`).color(0, 0x69A4C4)
 
-    event.create(`${tool}_head_${material}`).displayName(`${displayMaterial} ${displaytool} Head`).texture(`kubejs:item/tools/${tool}`).color(0, 0x69A4C4)
-           
-            })        
-        })            
     })
+})   
+
+//====================================================================================================================================================================\\
+
+
+QuickMetalChange.forEach(material => {
+    toolBlade.forEach(tb => {
+        let displaytoolswithBlade = capfirst(`${tb}`)
+        let displayMaterial = capfirst(`${material}`)
+
+    event.create(`${tb}_blade_${material}`).displayName(`${displayMaterial} ${displaytoolswithBlade} Blade`).texture(`kubejs:item/tools/${tb}`).color(0, 0x69A4C4)
+
+    })
+})  
+
+//====================================================================================================================================================================\\
+
+
+QuickMetalChange.forEach(material => {
+    toolWithout.forEach(tw => {
+        let displaytoolsWithout = capfirst(`${tw}`)
+        let displayMaterial = capfirst(`${material}`)
+    event.create(`${tw}_${material}`).displayName(`${displayMaterial} ${displaytoolsWithout}`).texture(`kubejs:item/tools/${tw}`).color(0, 0x69A4C4)
+
+        })
+    })     
+})
